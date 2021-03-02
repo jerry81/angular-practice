6b1129e2b883c7d8be07cf71dbf087ae872ab84a
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-02 14:37:53 +0800

    feat: add detail route

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../heroes/interfaces/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../heroes/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,9 +13,22 @@ import { Hero } from '../heroes/interfaces/hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero // property binding
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero()
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id') // getting param from route 
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
