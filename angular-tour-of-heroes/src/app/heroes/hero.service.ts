bf62ad474780ae26a591e64a35d87c67007ea070
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-06 20:04:28 +0800

    feat: update service to use http client

--- angular-tour-of-heroes/src/app/heroes/hero.service.ts
+++ angular-tour-of-heroes/src/app/heroes/hero.service.ts
@@ -3,22 +3,29 @@ import { Hero } from '../heroes/interfaces/hero'
 import { HEROES } from '../heroes/mock/mock-heroes'
 import { Observable, of } from 'rxjs'
 import { MessageService } from '../message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({ // participates in dependency injection system 
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient,
+  ) { } // service in service scenario
 
-  constructor(private messageService: MessageService) { } // service in service scenario
-
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    this.log(`HeroService: fetched hero id=${id}`)
     return of(HEROES.find(hero => hero.id === id))
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
